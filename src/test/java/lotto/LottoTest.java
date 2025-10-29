267e9c23606ed14116b84bd0c6dd956308e50554
haeseon lee <haeseon4816@gmail.com>
2025-10-29 20:13:35 +0900

    test: 로또 번호 범위 검증 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -21,6 +21,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호가 1 ~ 45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void 로또_번호_범위_벗어나면_예외_발생() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
