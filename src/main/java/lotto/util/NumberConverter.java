1eb62d34f1e3790ded90ea0eee61f7d6687124fe
haeseon lee <haeseon4816@gmail.com>
2025-10-29 14:59:03 +0900

    feat: 입력받은 문자열 숫자 변환 기능 구현

---
+++ src/main/java/lotto/util/NumberConverter.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+public class NumberConverter {
+
+    private NumberConverter() {
+    }
+
+    public static int convert(String input) {
+        return Integer.parseInt(input);
+    }
+}
