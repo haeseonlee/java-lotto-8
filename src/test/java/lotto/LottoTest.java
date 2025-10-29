a436d03b3e5670ea554b8a0ddc569f7d23b9f55c
haeseon lee <haeseon4816@gmail.com>
2025-10-29 15:43:33 +0900

    test: 문자열 입력값 숫자 변환 메서드 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,9 +3,12 @@ package lotto;
 import lotto.util.NumberConverter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -20,6 +23,14 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("문자열 입력값을 숫자로 변환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"8000", "7"})
+    void 문자열_입력값을_숫자로_변환한다(String text) {
+        int result = NumberConverter.convert(text);
+        assertThat(result).isEqualTo(Integer.parseInt(text));
     }
 
     @DisplayName("구입 금액이 비어있으면 예외가 발생한다.")
