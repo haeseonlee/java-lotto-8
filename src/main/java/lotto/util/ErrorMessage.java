84eb99b5526733a913f9daeda1b278b41c4a9a4c
haeseon lee <haeseon4816@gmail.com>
2025-10-29 14:47:58 +0900

    feat: 에러 메세지 관리 기능 구현

---
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+public enum ErrorMessage {
+    ;
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return "[ERROR] " + message;
+    }
+}
