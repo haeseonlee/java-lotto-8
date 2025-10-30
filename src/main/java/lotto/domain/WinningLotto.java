80c2c70428b65574311e40e1de9a854237a3babd
haeseon lee <haeseon4816@gmail.com>
2025-10-30 15:40:05 +0900

    refactor: 당첨 번호와 보너스 번호 중복 검증 메서드 리팩터링

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,20 +1,33 @@
 package lotto.domain;
 
 import lotto.util.ErrorMessage;
+import lotto.util.LottoConstants;
 
 import java.util.List;
 
+import static lotto.util.LottoConstants.*;
+
 public class WinningLotto {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
 
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        validateBonusNumberRange(bonusNumber);
+        validateDuplicateBonusNumber(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
+    private void validateBonusNumberRange(int bonusNumber) {
+        if (bonusNumber < MIN_RANGE_NUMBER || bonusNumber > MAX_RANGE_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBER_RANGE.getMessage());
+        }
+    }
 
+    private void validateDuplicateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_BONUS_NUMBER.getMessage());
+        }
+    }
 }
