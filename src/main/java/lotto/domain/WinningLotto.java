8614b78a712d9afe2d7e8ea5e07048619aafb375
haeseon lee <haeseon4816@gmail.com>
2025-10-30 11:33:18 +0900

    feat: 당첨 번호 중복 검증 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -16,6 +16,12 @@ public class WinningLotto {
         this.bonusNumber = bonusNumber;
     }
 
+    private void validateDuplicateNumber(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != winningNumbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
+        }
+    }
+
     private void validateNumberRage(int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBER_RANGE.getMessage());
