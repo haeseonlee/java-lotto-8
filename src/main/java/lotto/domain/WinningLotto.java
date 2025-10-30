2666bc4521ceed6747481546bf997b2191dc57c1
haeseon lee <haeseon4816@gmail.com>
2025-10-30 11:05:25 +0900

    feat: 당첨 번호, 보너스 번호 저장 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto {
+
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+}
