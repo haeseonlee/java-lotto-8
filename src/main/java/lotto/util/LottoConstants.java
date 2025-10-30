7812d158de525977159c62f2db81076f70436028
haeseon lee <haeseon4816@gmail.com>
2025-10-30 15:00:16 +0900

    feat: 로또 검증 클래스 구현

---
+++ src/main/java/lotto/util/LottoConstants.java
@@ -0,0 +1,12 @@
+package lotto.util;
+
+public class LottoConstants {
+
+    private LottoConstants() {
+    }
+
+    public static final int MIN_RANGE_NUMBER = 1;
+    public static final int MAX_RAGE_NUMBER = 45;
+    public static final int REGULAR_SIZE = 6;
+
+}
