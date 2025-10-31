d583181034c7f0487ae9b8376543c8a9e719435c
haeseon lee <haeseon4816@gmail.com>
2025-10-31 15:24:33 +0900

    feat: 당첨 통계 출력 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,10 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -13,6 +15,14 @@ public class OutputView {
     public void printAllLottos(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.toString());
+        }
+    }
+
+    public void printWinningStatistics(Map<LottoRank, Integer> result) {
+        System.out.println("당첨 통계\n---");
+        for (LottoRank rank : LottoRank.values()) {
+            Integer count = result.getOrDefault(rank, 0);
+            System.out.println(rank.getMessage() + " - " + count + "개");
         }
     }
 }
