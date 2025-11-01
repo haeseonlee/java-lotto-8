135735818bd4c0c721fbacf4ec12145febd15207
haeseon lee <haeseon4816@gmail.com>
2025-11-01 11:38:24 +0900

    refactor: 로또 결과 반환 타입 변경 리팩터링

--- src/main/java/lotto/service/LottoResult.java
+++ src/main/java/lotto/service/LottoResult.java
@@ -4,14 +4,14 @@ import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
 import lotto.domain.WinningLotto;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class LottoResult {
 
     public Map<LottoRank, Integer> getLottoResult(WinningLotto winningLotto, List<Lotto> lottos) {
-        Map<LottoRank, Integer> stats = new HashMap<>();
+        Map<LottoRank, Integer> stats = new EnumMap<>(LottoRank.class);
         for (Lotto lotto : lottos) {
             LottoRank rank = LottoMatch.match(winningLotto, lotto);
             stats.put(rank, stats.getOrDefault(rank, 0) + 1);
