eed9446210807c52f95a30b97ea8e5c12fd5632d
haeseon lee <haeseon4816@gmail.com>
2025-11-01 11:20:59 +0900

    refactor: 당첨 번호, 보너스 번호 검증 테스트 이동 리팩터링

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -110,44 +110,11 @@ class LottoTest {
                 .allMatch(Objects::nonNull);
     }
 
-    @DisplayName("당첨 번호 내에 보너스 번호와 같은 숫자가 존재하면 예외가 발생한다.")
-    @Test
-    void 당첨_번호와_보너스번호_겹치면_예외_발생() {
-
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 6))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("보너스 번호가 1 ~ 45 이내의 범위를 넘어가면 예외가 발생한다.")
-    @Test
-    void 보너스_번호_정해진_범위_초과시_예외_발생() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 70))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("당첨 번호와 발행한 로또 번호가 몇 개 일치하는지 반환한다.")
-    @Test
-    void 당첨번호와_로또번호_일치하는_번호있으면_개수_반환() {
-        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
-
-        assertThat(winningLotto.countMatchingNumbers(lotto)).isEqualTo(3);
-    }
-
-    @DisplayName("보너스 번호가 로또 번호에 포함되는지 여부를 반환한다.")
-    @Test
-    void 보너스번호_로또번호_포함여부_반환() {
-        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 7, 9, 10));
-
-        assertThat(winningLotto.isBonusMatch(lotto)).isEqualTo(true);
-    }
-
     @DisplayName("당첨 번호와 로또 번호를 비교해 일치하는 당첨 등수를 반환한다")
     @Test
     void 일치하는_당첨_등수_반환() {
         WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
         assertThat(LottoMatch.match(winningLotto, lotto)).isEqualTo(LottoRank.MATCH6);
     }
